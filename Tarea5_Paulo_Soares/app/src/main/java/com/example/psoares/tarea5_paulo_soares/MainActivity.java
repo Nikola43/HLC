98929a2040e6fb45b08418238801a45e4adafef3
Paulo Soares <pauloxti@gmail.com>
2019-01-10 14:53:25 +0100

    autocommit

--- Tarea5_Paulo_Soares/app/src/main/java/com/example/psoares/tarea5_paulo_soares/MainActivity.java
+++ Tarea5_Paulo_Soares/app/src/main/java/com/example/psoares/tarea5_paulo_soares/MainActivity.java
@@ -11,17 +11,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button botonCalcular = findViewById(R.id.button_calcular);
+    // Creamos el boton calcular y el spinner
+    Button botonCalcular;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Enlazamos los objetos creados con la ui
+        botonCalcular = findViewById(R.id.button_calcular);
+        spinner = findViewById(R.id.spinner);
+
         botonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Spinner spinner = findViewById(R.id.spinner);
+                // cogemos el texto de la opcion seleccionada
                 String elementoSeleccionado = spinner.getSelectedItem().toString();
                 calcular(elementoSeleccionado);
             }
@@ -31,22 +37,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void calcular(String operacion){
-        float calculo = 0;
+        double calculo = 0;
         EditText campo1    = findViewById(R.id.campo1);
         EditText campo2    = findViewById(R.id.campo2);
         TextView resultado = findViewById(R.id.resultado);
 
-        if (campo1.getText().toString().length() > 0 && campo2.getText().toString().length() > 0){
+        // Si los dos campos y no estan vacios
+        // y si son mayor que 0
+        if (campo1.getText().toString().length() > 0 && campo2.getText().toString().length() > 0 &&
+            Float.parseFloat(campo1.getText().toString()) > 0 && Float.parseFloat(campo2.getText().toString()) > 0) {
 
+            // segun la operacion seleccionar suma, resta, multiplica o divide
             switch (operacion){
-                case "Sumar" : calculo = Float.parseFloat(campo1.getText().toString()) + Float.parseFloat(campo2.getText().toString()); break;
-                case "Restar" : calculo = Float.parseFloat(campo1.getText().toString()) - Float.parseFloat(campo2.getText().toString()); break;
+                case "Sumar" :       calculo = Float.parseFloat(campo1.getText().toString()) + Float.parseFloat(campo2.getText().toString()); break;
+                case "Restar" :      calculo = Float.parseFloat(campo1.getText().toString()) - Float.parseFloat(campo2.getText().toString()); break;
                 case "Multiplicar" : calculo = Float.parseFloat(campo1.getText().toString()) * Float.parseFloat(campo2.getText().toString()); break;
-                case "Dividir" : calculo = Float.parseFloat(campo1.getText().toString()) / Float.parseFloat(campo2.getText().toString()); break;
+                case "Dividir" :     calculo = Float.parseFloat(campo1.getText().toString()) / Float.parseFloat(campo2.getText().toString()); break;
+                case "Potencia" :    calculo = Math.pow(Float.parseFloat(campo1.getText().toString()) , Float.parseFloat(campo2.getText().toString())); break;
             }
-            resultado.setText(String.valueOf(calculo));
+            resultado.setText("Resultado: "+ String.valueOf(calculo));
         } else{
-            Toast.makeText(getApplicationContext(), "Debe introducir algun numero en el campo 1 y 2", Toast.LENGTH_SHORT).show();
+            if (campo1.getText().toString().length() == 0 && campo2.getText().toString().length() == 0)
+                Toast.makeText(getApplicationContext(), "Debe introducir algun numero en el campo 1 y 2", Toast.LENGTH_SHORT).show();
+
+            if (Float.parseFloat(campo1.getText().toString()) > 0 && Float.parseFloat(campo2.getText().toString()) > 0)
+                Toast.makeText(getApplicationContext(), "Los valores del campo1 y campo2 debe ser mayor que 0", Toast.LENGTH_SHORT).show();
         }
     }
 
